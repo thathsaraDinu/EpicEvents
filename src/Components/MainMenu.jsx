769e33abccab873535f0b3dea3ef0b138a3c2b84
thathsaraDinu <it22306968@my.sliit.lk>
2024-07-21 21:10:26 +0530

    menu adjust

--- src/Components/MainMenu.jsx
+++ src/Components/MainMenu.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 export default function MainMenu() {
   return (
     <div className="nav z-69">
-      <ul className="flex lg:flex-row flex-col lg:fixed  justify-start lg:justify-between py-3 bg-white z-50  px-5 font-medium  shadow-lg w-full">
-        <Link className="w-full lg:w-auto mb-5 lg:mb-0 " to="/">
+      <ul className="flex sm:flex-row flex-col lg:fixed  justify-start sm:justify-between  py-3 bg-white z-50  px-5 font-medium  shadow-lg w-full">
+        <Link className="w-full sm:w-auto mb-5 sm:mb-0 " to="/">
           <img
             id="logo"
             src="/epic-events-high-resolution-logo-transparent.png"
@@ -13,7 +13,7 @@ export default function MainMenu() {
           />
         </Link>
 
-        <div className="flex flex-wrap gap-6 items-center">
+        <div className="flex flex-wrap sm: gap-6 items-center  md:justify-end">
           <Link to={"/"} className="w-full sm:w-auto mx-3 hover:pointer">
             Home
           </Link>
@@ -22,24 +22,23 @@ export default function MainMenu() {
           </Link>
           <Link
             to={"/createevent"}
-            className="w-full sm:w-auto mx-3 hover:pointer"
+            className="w-full sm:w-auto mx-3 hover:pointer sm:hidden lg:block "
           >
             Create Event
           </Link>
-          <Link to={"/"} className="w-full sm:w-auto mx-3 hover:pointer">
+          <Link
+            to={"/"}
+            className="w-full sm:w-auto mx-3 hover:pointer sm:hidden md:block "
+          >
             Contact Us
           </Link>
           <Link to={"/"} className="w-full sm:w-auto mx-3 hover:pointer">
-            <button className="hover:bg-gray-200 py-2 px-4 border-2 border-black rounded-lg" >
+            <button className="hover:bg-gray-200 py-2 px-4 border-2 border-black rounded-lg">
               Sign In
             </button>
           </Link>
         </div>
       </ul>
-
-     
-        
-      </div>
-    
+    </div>
   );
 }
